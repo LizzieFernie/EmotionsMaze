da1ab09d8b24bf90d429369b7b7deb9eaa8f38c3
fernandeslizanne <53610361+LizzieFernie@users.noreply.github.com>
2021-07-07 20:31:20 +0530

    Add files via upload

---
+++ Button/Button.js
@@ -0,0 +1,40 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Button extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("b1", "./Button/costumes/b1.svg", {
+        x: 239.9999999999999,
+        y: 180.49999999999997
+      })
+    ];
+
+    this.sounds = [];
+
+    this.triggers = [
+      new Trigger(Trigger.CLICKED, this.whenthisspriteclicked),
+      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)
+    ];
+  }
+
+  *whenthisspriteclicked() {
+    this.visible = false;
+    this.stage.costume = 1;
+    this.broadcast("instrWindow");
+  }
+
+  *whenGreenFlagClicked() {
+    this.visible = true;
+  }
+}
